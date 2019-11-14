5fe7c1ce11c093f0da4670aa5d4ea2703d2f196e
Simon Aagaard <simo136g@edu.eal.dk>
2019-11-14 09:34:42 +0100

    init model and dbcontext setup

---
+++ Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Data.Entities;
+
+namespace Data
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users {get; set;}
+        public DbSet<Dashboard> Dashboards { get; set; }
+
+    }
+}
---
+++ Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
