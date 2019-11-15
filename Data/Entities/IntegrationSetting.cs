c41a9e912bc126d51c4ea0a61cdef8ff9eb5a608
Simon Aagaard <simo136g@edu.eal.dk>
2019-11-15 12:29:46 +0100

    Updated models with data annotations

--- Data/Entities/IntegrationSetting.cs
+++ Data/Entities/IntegrationSetting.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Data.Entities
 {
     public class IntegrationSetting
     {
+        [Key]
         public Guid IntegrationSettingId { get; set; }
+        [ForeignKey("IntegrationId")]
         public Guid IntegrationId { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
