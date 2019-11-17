b4c866624d2a8d39d573a1902941847c8578d22c
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2019-11-17 14:46:40 +0100

    AzureConnector class added. No functionality yet

---
+++ Data/Integrations/Connectors/AzureConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Integrations
+{
+    class AzureConnector
+    {
+        private readonly string _clientId = "";
+        private readonly string _clientSecret = "";
+        private readonly string _tenantId = "";
+
+        public AzureConnector(string clientId, string clientSecret, string tenantId)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _tenantId = tenantId;
+        }
+    }
+}
