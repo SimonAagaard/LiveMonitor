07695815ebce8249465dd7b4c34406b0d5199518
Lasse Duus Skjønnemann <lass8293@edu.eal.dk>
2019-11-21 10:05:04 +0100

    Naming convention

--- Data/Handlers/DataSetHandler.cs
+++ Data/Handlers/DataSetHandler.cs
@@ -11,51 +11,51 @@ namespace Data.Handlers
 
     public class DataSetHandler
     {
-        private readonly Repository<DataSet> dataSetRepo;
+        private readonly Repository<DataSet> _dataSetRepo;
 
         public DataSetHandler()
         {
-            dataSetRepo = new Repository<DataSet>();
+            _dataSetRepo = new Repository<DataSet>();
         }
 
-        public async Task CreateDataSet(DataSet DataSet)
+        public async Task CreateDataSet(DataSet dataSet)
         {
-            await dataSetRepo.Add(DataSet);
+            await _dataSetRepo.Add(dataSet);
         }
 
         // Get all DataSets in the database
         public async Task<List<DataSet>> GetDataSets()
         {
-            return await dataSetRepo.GetAll();
+            return await _dataSetRepo.GetAll();
         }
 
         // Get a single DataSet based on the dataSetId
-        public async Task<DataSet> GetDataSet(Guid DataSetId)
+        public async Task<DataSet> GetDataSet(Guid dataSetId)
         {
-            return await dataSetRepo.Get(DataSetId);
+            return await _dataSetRepo.Get(dataSetId);
         }
 
         // Get dataset based on their IntegrationSettingId
         public async Task<List<DataSet>> GetDataSetsByIntegrationSettingId(Guid integrationSettingId)
         {
-            return await dataSetRepo.GetMany(x => x.IntegrationSettingId == integrationSettingId);
+            return await _dataSetRepo.GetMany(x => x.IntegrationSettingId == integrationSettingId);
         }
 
         public async Task<List<DataSet>> GetDataSetsFromAGivenTimePeriod(Guid integrationSettingId, DateTime fromDate, DateTime toDate)
         {
-            return await dataSetRepo.GetMany(x => x.IntegrationSettingId == integrationSettingId && x.DateCreated >= fromDate && x.DateCreated <= toDate );
+            return await _dataSetRepo.GetMany(x => x.IntegrationSettingId == integrationSettingId && x.DateCreated >= fromDate && x.DateCreated <= toDate );
         }
 
         // Update a DataSet object
         public async Task UpdateDataSet(DataSet dataSet)
         {
-            await dataSetRepo.Update(dataSet);
+            await _dataSetRepo.Update(dataSet);
         }
 
         // Hard delete a dataset based on the DataSetId
-        public async Task DeleteDataSet(Guid DataSetId)
+        public async Task DeleteDataSet(Guid dataSetId)
         {
-            await dataSetRepo.Delete(DataSetId);
+            await _dataSetRepo.Delete(dataSetId);
         }
 
     }
