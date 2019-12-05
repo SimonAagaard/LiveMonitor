34e4317242704478289b65f5659c0b785a5c799b
Simon Aagaard <simo136g@edu.eal.dk>
2019-12-05 11:54:13 +0100

    Min and max value for RefreshRate

--- Data/Entities/DashboardSetting.cs
+++ Data/Entities/DashboardSetting.cs
@@ -23,6 +23,7 @@ namespace Data.Entities
         public Guid DashboardTypeId { get; set; }
         [Required]
         [Display(Name = "Refresh rate")]
+        [Range(5, 86400)] //Min value to 5 seconds, max value to 24 hours
         public int RefreshRate { get; set; }
         [Display(Name = "X-axis Label")]
         [MaxLength(128)]
